9b68cfdadd208f95e42221dc1491cb17c0e3aa00
Nabeel Kahlil Maulana <nabeelkahlil403@gmail.com>
2023-03-25 12:47:05 +0700

    add conversation caching (#9)
    * add conversation caching
    
    * add time field on conversation caching
    
    * change stirng seperator format

--- polly/usecase/conversation.py
+++ polly/usecase/conversation.py
@@ -7,6 +7,12 @@ from polly.usecase.base import UseCase
 from polly.model.conversation import Conversation
 from polly.inject import ClientContainer
 
+from typing import List, Tuple
+
+MAX_CACHED_CONVERSATIONS = 5
+
+CONVERSATION_SEPERATOR = '|'
+CONVERSATION_FIELD_SEPERATOR = ';'
 
 class ConversationUC(UseCase):
 
@@ -31,3 +37,78 @@ class ConversationUC(UseCase):
             )
             session.execute(statement)
             session.commit()
+
+
+    def get_user_last_conversations(self, user_id: int) -> List[Tuple[str,str,int]] | List:
+        """
+        Return list of conversations in Tuple pair
+        """
+
+        conversations = self.cache_retrieve(user_id=user_id)
+        if not conversations:
+            return None
+        
+        return [
+            self._decode_conversation(conv)
+            for conv in conversations.split(CONVERSATION_SEPERATOR)
+        ]
+
+
+    def cache_insert(self, conversation: Conversation, user_id: int) -> None:
+        """
+        # Conversations Encoding Format
+        
+        ## Conversations -> use `;`
+        "conversation;conversation;conversation"
+
+        ## Conversation / Chat -> use `:`
+        "user_message:chat_response"
+
+        """
+        cached_conversations = self.cache_retrieve(user_id=user_id)
+
+        if cached_conversations:
+            conversationList: List[str] = cached_conversations.split(';')
+            if len(conversationList) > MAX_CACHED_CONVERSATIONS :
+                conversationList.pop()
+        else:
+            conversationList = []
+
+        encoded = self._encode_conversation(
+            user_message=conversation.user_message,
+            chat_response=conversation.chat_response,
+            time=conversation.created_at,
+        )
+        conversationList.insert(0, encoded)
+        updated_conversations = CONVERSATION_SEPERATOR.join(conversationList)
+
+        self.cache_save(user_id, updated_conversations)
+
+
+    def cache_retrieve(self, user_id: int) -> str | None:
+        key = self._redis_key_format(user_id)
+        conversations = self.cache.get(key)
+        return conversations
+
+
+    def cache_save(self, user_id: int, conversations: str) -> None:
+        key = self._redis_key_format(user_id)
+        self.cache.set(key, conversations, self.cache.ONE_DAY)
+
+
+    @classmethod
+    def _encode_conversation(cls, user_message: str, chat_response: str, time: int) -> str:
+        return f'{user_message}{CONVERSATION_FIELD_SEPERATOR}{chat_response}{CONVERSATION_FIELD_SEPERATOR}{time}'
+
+
+    @classmethod 
+    def _decode_conversation(cls, encoded: str) -> Tuple[str, str, int]:
+        splits = encoded.split(CONVERSATION_FIELD_SEPERATOR)
+        message, response, time = splits[0], splits[1], splits[2]
+        return  message, response, int(time)
+
+
+    @classmethod
+    def _redis_key_format(cls, user_id: int) -> str:
+        return f'{user_id}:conversations'
+    
